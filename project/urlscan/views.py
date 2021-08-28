c1a0e37547f900dc2e7941fc5ee14c964eb9975b
Sciratec <84640654+Sciratec@users.noreply.github.com>
2021-08-27 22:50:30 -0400

    Added more error handling, and minor updates

--- project/urlscan/views.py
+++ project/urlscan/views.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, Blueprint, request, redirect
 import requests, json, time
 
-from requests import api
-
 urlscan_blueprint = Blueprint('urlscan', __name__, template_folder='templates')
 
 @urlscan_blueprint.route('/urlscan', methods=['GET','POST'])
@@ -15,7 +13,7 @@ def urlscan():
         if scan:
 
             headers = {
-            'API-Key':'$APIKEY', #Supply own key 
+            'API-Key':'$APIKEY', # Supply own key
             'Content-Type':'application/json'
             }
 
@@ -31,19 +29,26 @@ def urlscan():
                 phish = "Yes"
                 apiResult = requests.get(jsonLoads['api'], headers=headers)
                 apiLoads = json.loads(apiResult.text)
-                print(apiLoads)
+                if "message" in apiLoads and apiLoads['status'] != 200:
+                    while "message" in apiLoads and apiLoads['status'] != 200:
+                        time.sleep(30)
+                        apiResult = requests.get(jsonLoads['api'], headers=headers)
+                        apiLoads = json.loads(apiResult.text)
+                        if "message" not in apiLoads:
+                            break
                 if "ip" in apiLoads['page']:
                     verdicts = apiLoads['verdicts']
                     url = apiLoads['page']['url']
                     ip = apiLoads['page']['ip']
                     asn = apiLoads['page']['asnname']
+                    img = apiLoads['task']['screenshotURL']
                     if "phishing" in verdicts['overall']['tags'] or "phishing" in verdicts['urlscan']['tags'] or "phishing" in verdicts['community']['tags']:
-                        return render_template('scanresults.html', url=url, ip=ip, asn=asn,phish=phish)
+                        return render_template('scanresults.html', url=url, ip=ip, asn=asn,phish=phish, img=img)
                     else:
                         clean = "No"
-                        return render_template('scanresults.html', clean=clean, url=url)
+                        return render_template('scanresults.html', clean=clean, url=url, img=img)
                 else: 
-                    noIP = "No IP"
+                    noIP = f"There is no IP associated with {scan}"
                     return render_template('scanresults.html', noIP=noIP)
             else:
                 blacklistCheck = jsonLoads['description']
